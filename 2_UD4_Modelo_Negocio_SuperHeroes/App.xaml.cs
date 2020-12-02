f5b14ba59af511fed83312bb1e2812f1aa8fbb26
Manuel <ma.gonzalez16@iesdoctorbalmis.com>
2020-12-02 19:37:34 +0100

    Agregar archivos de proyecto.

---
+++ 2_UD4_Modelo_Negocio_SuperHeroes/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _2_UD4_Modelo_Negocio_SuperHeroes
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ 2_UD4_Modelo_Negocio_SuperHeroes/FondoConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace _2_UD4_Modelo_Negocio_SuperHeroes
+{
+    class FondoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if((Boolean)value)
+            {
+                return new SolidColorBrush(Colors.PaleGreen);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.IndianRed);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
